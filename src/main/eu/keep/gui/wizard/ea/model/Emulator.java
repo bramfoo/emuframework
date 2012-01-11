714f3442ccb18240137829601ff02d48a8db1666
bkiers <bart@big-o.nl>
2012-01-11 10:23:32 +0000

    Added some missing class-headers
    git-svn-id: svn+ssh://svn.code.sf.net/p/emuframework/code/Core/trunk@189 ed3a90e0-80d1-4e3e-b80e-4541647673b9

--- src/main/eu/keep/gui/wizard/ea/model/Emulator.java
+++ src/main/eu/keep/gui/wizard/ea/model/Emulator.java
@@ -1,3 +1,33 @@
+/*
+ * $Revision: 114 $ $Date: 2011-12-01 12:25:35 +0100 (Thu, 01 Dec 2011) $
+ * $Author: bkiers $
+ * $header:
+ * Copyright (c) 2009-2011 Tessella plc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information about this project, visit
+ *   http://www.keep-project.eu/
+ *   http://emuframework.sourceforge.net/
+ * or contact us via email:
+ *   blohman at users.sourceforge.net
+ *   dav_m at users.sourceforge.net
+ *   bkiers at users.sourceforge.net
+ * Developed by:
+ *   Tessella plc <www.tessella.com>
+ *   Koninklijke Bibliotheek <www.kb.nl>
+ *   KEEP <www.keep-project.eu>
+ * Project Title: Core Emulation Framework (Core EF)$
+ */
 package eu.keep.gui.wizard.ea.model;
 
 import java.io.File;
