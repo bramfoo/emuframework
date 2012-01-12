6db32ce1e4b8f7ac83f1dc52731f35a568a742bc
bkiers <bart@big-o.nl>
2012-01-12 09:40:23 +0000

    Added software remove-wizard.
    git-svn-id: svn+ssh://svn.code.sf.net/p/emuframework/code/Core/trunk@202 ed3a90e0-80d1-4e3e-b80e-4541647673b9

--- src/main/eu/keep/gui/wizard/swa/SWAWizardRemove.java
+++ src/main/eu/keep/gui/wizard/swa/SWAWizardRemove.java
@@ -30,17 +30,95 @@
  */
 package eu.keep.gui.wizard.swa;
 
+import eu.keep.gui.util.DBUtil;
+import eu.keep.gui.wizard.swa.model.Img;
+import org.apache.log4j.Logger;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
 
 public class SWAWizardRemove extends JFrame {
+
+    private static Logger logger = Logger.getLogger(DBUtil.class.getName());
 
     public SWAWizardRemove() {
         super("Software Archive wizard");
 
         super.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         super.setResizable(false);
-        super.setSize(500, 600);
+        super.setSize(500, 100);
+
+        initGUI();
 
         super.setVisible(true);
+    }
+
+    private void initGUI() {
+        Vector<Vector<String>> swData = DBUtil.query(DBUtil.DB.SWA,
+                "SELECT image_id,description, imageformat_id, platform_id " +
+                "FROM softwarearchive.images"
+        );
+        Vector<Img> swVec = new Vector<Img>();
+        for(Vector<String> row : swData) {
+            swVec.add(new Img(row.get(0), row.get(1), row.get(2), row.get(3)));
+        }
+
+        super.setLayout(new BorderLayout(5, 5));
+        super.add(new JLabel(" "), BorderLayout.NORTH);
+        super.add(new JLabel(" "), BorderLayout.WEST);
+        super.add(new JLabel(" "), BorderLayout.EAST);
+        super.add(new JLabel(" "), BorderLayout.SOUTH);
+
+        final JPanel mainPanel = new JPanel(new BorderLayout(5, 5));
+        final JComboBox swCombo = new JComboBox(swVec);
+        final JButton delete = new JButton("delete");
+
+        mainPanel.add(swCombo, BorderLayout.CENTER);
+        mainPanel.add(delete, BorderLayout.EAST);
+
+        delete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                final Img img = (Img) swCombo.getSelectedItem();
+
+                Object[] options = {"Yes", "Cancel"};
+                int returnValue = JOptionPane.showOptionDialog(SWAWizardRemove.this,
+                        "Are you sure you want to remove " + img + "?\n This operation cannot be undone!",
+                        "Delete software",
+                        JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
+
+                if (returnValue == JOptionPane.YES_OPTION) {
+                    (new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                DBUtil.update(DBUtil.DB.SWA, "DELETE FROM softwarearchive.opsys_images WHERE image_id=?", img.image_id);
+                                DBUtil.update(DBUtil.DB.SWA, "DELETE FROM softwarearchive.apps_images WHERE image_id=?", img.image_id);
+                                DBUtil.update(DBUtil.DB.SWA, "DELETE FROM softwarearchive.imageblobs WHERE image_id=?", img.image_id);
+                                DBUtil.update(DBUtil.DB.SWA, "DELETE FROM softwarearchive.images WHERE image_id=?", img.image_id);
+
+                                JOptionPane.showMessageDialog(SWAWizardRemove.this,
+                                        "Successfully deleted: " + swCombo.getSelectedItem(),
+                                        "Error",
+                                        JOptionPane.INFORMATION_MESSAGE);
+                            } catch (Exception ex) {
+                                JOptionPane.showMessageDialog(SWAWizardRemove.this,
+                                        "Could not remove " + swCombo.getSelectedItem() + ": " + ex.getMessage(),
+                                        "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                            }
+                        }
+                    })).start();
+
+                    SWAWizardRemove.this.dispose();
+                }
+            }
+        });
+
+        super.add(mainPanel, BorderLayout.CENTER);
     }
 }
